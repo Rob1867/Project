311e9899909a78fda08474b9ef983a669782ef67
Rob1867 <robert.everitt@nationalgrid.com>
2025-11-06 17:16:23 +0000

    Added gx

---
+++ gx/run_gx.py
@@ -0,0 +1,57 @@
+
+import pandas as pd
+
+vi_filepath = r'C:\Users\robert.everitt\OneDrive - National Grid\Data Engineering Course\Data\VI_E0309B_Test.xlsx'
+
+df_vi = pd.read_excel(vi_filepath)
+
+def clean_dataframe(df, search_text):
+
+    # Find the index of the first occurrence of the search text
+    idx = df[df.apply(lambda row: row.astype(str).str.contains(search_text, case=False).any(), axis=1)].index
+    
+    if idx.empty:
+        raise ValueError(f"'{search_text}' not found in the DataFrame.")
+    
+    # Get the first matching index
+    first_match_idx = idx[0]
+    
+    # Update the DataFrame by removing rows above the match
+    df = df.iloc[first_match_idx:].reset_index(drop=True)
+    
+    # Set the first row as column names and drop it from the DataFrame
+    df.columns = df.iloc[0]
+    df = df[1:].reset_index(drop=True)
+    
+    return df
+
+df_vi_clean = clean_dataframe(df_vi,'Location Code')
+
+df_vi_clean.to_csv('out.csv', index=False)  
+
+import great_expectations as gx
+import great_expectations.expectations as gxe
+
+context = gx.get_context(mode="file", context_root_dir=".")
+
+data_source = context.data_sources.add_or_update_pandas_filesystem(
+    name="data_source", base_directory=".."
+)
+
+data_asset = data_source.add_csv_asset(name="data_asset")
+
+batch_def = data_asset.add_batch_definition_path(
+    name="batch_def", path="out.csv"
+)
+
+suite = context.suites.add_or_update(gx.ExpectationSuite(name="sales_data_suite"))
+
+# TODO: Add more expectations
+suite.add_expectation(gxe.ExpectColumnValuesToBeUnique(column="Script Activity ID"))
+#suite.add_expectation(gxe.ExpectColumnValuesToNotBeNull(column="price_per_unit"))
+#suite.add_expectation(gxe.ExpectColumnValuesToBeBetween(column="quantity",min_value=1.0,max_value=None))
+
+batch = batch_def.get_batch()
+print(batch.head())
+
+print(batch.validate(suite))
