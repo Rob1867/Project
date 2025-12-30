b635e7a22b14d03bda4999c7e287649c136f4bd0
Rob1867 <robert.everitt@nationalgrid.com>
2025-12-30 10:48:54 +0000

    Included assets not avaialble

--- gx/run_gx.py
+++ gx/run_gx.py
@@ -35,7 +35,7 @@ import great_expectations.expectations as gxe
 context = gx.get_context(mode="file", context_root_dir=".")
 
 data_source = context.data_sources.add_or_update_pandas_filesystem(
-    name="data_source", base_directory=".."
+    name="data_source", base_directory="./gx"
 )
 
 data_asset = data_source.add_csv_asset(name="data_asset")
@@ -44,14 +44,23 @@ batch_def = data_asset.add_batch_definition_path(
     name="batch_def", path="out.csv"
 )
 
-suite = context.suites.add_or_update(gx.ExpectationSuite(name="sales_data_suite"))
+suite = context.suites.add_or_update(gx.ExpectationSuite(name="VI_data_suite"))
 
 # TODO: Add more expectations
-suite.add_expectation(gxe.ExpectColumnValuesToBeUnique(column="Script Activity ID"))
-#suite.add_expectation(gxe.ExpectColumnValuesToNotBeNull(column="price_per_unit"))
+suite.add_expectation(gxe.ExpectColumnValuesToNotBeNull(column="Script Activity ID"))
+
+Installed_componets = ["VT", "PI", "DIS", "ESW", "ERT", "CSE", "CT", "TAP", "HVB", "GCB", "SGT", "SA", "BLDS", "VCT", "ABC", "NOI",
+    "LVB", "FNCE", "GAN", "ROAD", "TRCH", "OCB", "BUS", "EAT", "BUC", "BUA", "BUB", "COM", "PNL", "AUX", "QB",
+    "REA", "ENV", "GZ", "0", "TXM", "MSC", "SPI", "CT", "BB", "GEN", "HES", "CBR", "LIT", "FIL"
+]
+
+suite.add_expectation(gxe.ExpectColumnDistinctValuesToBeInSet(column="Installed Component",value_set=Installed_componets))
 #suite.add_expectation(gxe.ExpectColumnValuesToBeBetween(column="quantity",min_value=1.0,max_value=None))
 
 batch = batch_def.get_batch()
 print(batch.head())
 
 print(batch.validate(suite))
+
+context.build_data_docs()
+context.open_data_docs()
